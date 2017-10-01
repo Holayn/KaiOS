09526390d5488ec64f3bf6c4707915e34221f8a2
Kai Wong <kai.wong@ibm.com>
2017-10-01 02:16:42 -0400

    Added limit to memory manager and renamed partitions to base

--- distrib/os/memoryManager.js
+++ distrib/os/memoryManager.js
@@ -6,21 +6,22 @@
 var TSOS;
 (function (TSOS) {
     var MemoryManager = /** @class */ (function () {
-        function MemoryManager(partitions) {
-            this.partitions = partitions;
-            this.partitions = [0, 256, 512];
+        function MemoryManager(base) {
+            this.base = base;
+            this.limit = 256;
+            this.base = [0, 256, 512];
         }
         // For now, we load programs into location 00 of memory, in the first partition.
         // Later, we'll have to change this
         MemoryManager.prototype.loadIntoMemory = function (opCodes) {
-            var loadCounter = this.partitions[0];
+            var loadCounter = this.base[0];
             for (var _i = 0, opCodes_1 = opCodes; _i < opCodes_1.length; _i++) {
                 var opCode = opCodes_1[_i];
                 _Memory.memoryArray[loadCounter] = opCode;
                 loadCounter++;
             }
-            // Fill rest of the partition with 0's
-            for (var i = loadCounter; i < this.partitions[1]; i++) {
+            // Fill rest of the partitions with 0's
+            for (var i = loadCounter; i < this.limit; i++) {
                 _Memory.memoryArray[i] = "00";
             }
             console.log(_Memory.memoryArray);
--- source/os/memoryManager.ts
+++ source/os/memoryManager.ts
@@ -6,20 +6,21 @@
 
    module TSOS {
     export class MemoryManager {
-        constructor(public partitions: Array<number>){
-            this.partitions = [0,256,512];
+        public limit: number = 256;
+        constructor(public base: Array<number>){
+            this.base = [0,256,512];
         }
         
         // For now, we load programs into location 00 of memory, in the first partition.
         // Later, we'll have to change this
         public loadIntoMemory(opCodes): void {
-            var loadCounter = this.partitions[0];
+            var loadCounter = this.base[0];
             for(var opCode of opCodes){
                 _Memory.memoryArray[loadCounter] = opCode;
                 loadCounter++;
             }
-            // Fill rest of the partition with 0's
-            for(var i=loadCounter; i<this.partitions[1]; i++){
+            // Fill rest of the partitions with 0's
+            for(var i=loadCounter; i<this.limit; i++){
                 _Memory.memoryArray[i] = "00";
             }
             console.log(_Memory.memoryArray);
