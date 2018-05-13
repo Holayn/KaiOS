9fa4f35a55512a26a9bb203485e05d9ed9a86d8a
Holayn <kwwong15@punahou.edu>
2018-05-13 01:33:56 -0400

    nvm future updates to my compiler will make this a pain to update

--- compiler/compile.bat
+++
@@ -1,2 +0,0 @@
-tsc --version
-tsc --rootDir source/ --outDir dist/  source/*.ts source/scripts/*.ts
--- compiler/dist/scripts/errortype.js
+++
@@ -1,7 +0,0 @@
-var TSC;
-(function (TSC) {
-    var ErrorType;
-    (function (ErrorType) {
-        ErrorType["InvalidToken"] = "InvalidToken";
-    })(ErrorType = TSC.ErrorType || (TSC.ErrorType = {}));
-})(TSC || (TSC = {}));
